b4799ec86a7a29093b6938cb26bbd0660fe9c45a
Tiago Nazario <tiagosouza552f@gmail.com>
2025-11-24 19:28:39 -0300

    24/11/2025

--- Exercicios/modulo 01/siteUnivesp/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists posts;
-
-create table posts(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    title TEXT NOT NULL,
-    content TEXT NOT NULL 
-); 
